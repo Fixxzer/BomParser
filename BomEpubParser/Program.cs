8342fa12e4a3b152e10c4d78b05bd827ae786fa8
Jacob Garner <Jacob.Garner@coxautoinc.com>
2018-02-21 10:53:28 -0700

    BomParser

---
+++ BomEpubParser/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace BomEpubParser
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            // This assumes a couple of things
+            // 1. epub has been unzipped to the location below
+
+            var files = Directory.GetFiles("C:\\Users\\jacob.garner\\Downloads\\_testCode\\xml");
+
+            foreach (var file in files)
+            {
+                var text = File.ReadAllText(file);
+                PrintChapter(text);
+            }
+        }
+
+        private static void PrintChapter(string text)
+        {
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(text);
+
+            var bookNode = htmlDocument.DocumentNode.SelectSingleNode("//p[@class='runHead']");
+            if (bookNode != null)
+            {
+                var root = "C:\\Users\\jacob.garner\\Downloads\\_testCode\\bomOutput\\";
+                var book = bookNode.InnerText;
+                var chapter = htmlDocument.DocumentNode.SelectSingleNode("//p[@class='titleNumber']").InnerText;
+
+                if (!Directory.Exists($"{root}{book}"))
+                {
+                    Directory.CreateDirectory($"{root}{book}");
+                }
+
+                StringBuilder sb = new StringBuilder();
+                var htmlNodeCollection = htmlDocument.DocumentNode.SelectNodes("//p[@class='verse-first'] | //p[@class='verse']");
+
+                foreach (HtmlNode node in htmlNodeCollection)
+                {
+                    sb.AppendLine(node.InnerText.Trim());
+                }
+
+                string chapterText = sb.ToString();
+                //Console.WriteLine(chapterText);
+                File.AppendAllText($"{root}{book}\\{chapter}.txt", chapterText);
+            }
+        }
+    }
+}
---
+++ OutputAnalyzer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace OutputAnalyzer
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            // Assumptions:
+            // 1. epub has been unzipped
+            // 2. BomEpubParser has parsed and stripped all the xml out of the files
+            // 3. Files to process are located at the hard coded location below
+
+            var root = "C:\\Users\\jacob.garner\\Downloads\\_testCode\\bomOutput";
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var dir in Directory.GetDirectories(root))
+            {
+                Dictionary<string, int> dict = new Dictionary<string, int>();
+                foreach (var file in Directory.GetFiles(dir))
+                {
+                    string allText = File.ReadAllText(file);
+                    string[] allWords = allText.Split(' ');
+
+                    foreach (var word in allWords)
+                    {
+                        if (!dict.ContainsKey(word))
+                        {
+                            dict.Add(word, 0);
+                        }
+
+                        dict[word]++;
+                    }
+                }
+
+                Console.WriteLine(Path.GetFileName(dir));
+                Console.WriteLine("-------------------");
+
+                sb.Append(Path.GetFileName(dir));
+
+                foreach (var orderedItem in dict.OrderByDescending(pair => pair.Value).Take(25))
+                {
+                    Console.WriteLine($"{orderedItem.Key}: {orderedItem.Value}");
+                    sb.AppendLine($",{orderedItem.Key},{orderedItem.Value}");
+                }
+
+                Console.WriteLine("-------------------");
+                //File.WriteAllText($"{root}\\output.csv", sb.ToString());
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
